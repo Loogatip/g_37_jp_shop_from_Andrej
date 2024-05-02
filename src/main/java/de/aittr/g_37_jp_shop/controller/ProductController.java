c722bce515d93d3f7533a24d2604c210f0f21f07
Andrey <k-udm@ya.ru>
2024-05-02 14:06:20 +0200

    lesson 12

--- src/main/java/de/aittr/g_37_jp_shop/controller/ProductController.java
+++ src/main/java/de/aittr/g_37_jp_shop/controller/ProductController.java
@@ -4,6 +4,8 @@ import de.aittr.g_37_jp_shop.domain.dto.ProductDto;
 import de.aittr.g_37_jp_shop.service.interfaces.ProductService;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/products")
 public class ProductController {
@@ -22,14 +24,27 @@ public class ProductController {
 //        return service.getById(id);
 //    }
 
-    // 2 способ: передача ID в виде параметра запроса
-    // GET - localhost:8080/products/example?id=5
+    // GET - localhost:8080/products/all
 
+    // Просматривать все продукты могут все пользователи,
+    // даже не зарегистрированные
+    @GetMapping("/all")
+    public List<ProductDto> getAll() {
+        return service.getAll();
+    }
+
+    // 2 способ: передача ID в виде параметра запроса
+    // GET - localhost:8080/products?id=5
+
+    // Обращаться к одному конкретному продукту
+    // может только зарегистрированный пользователь (с любой ролью)
     @GetMapping
     public ProductDto getById(@RequestParam Long id) {
         return service.getById(id);
     }
 
+    // Сохранять новый продукт в базе данных
+    // может только администратор (пользователь с ролью ADMIN)
     @PostMapping
     public ProductDto save(@RequestBody ProductDto product) {
         return service.save(product);
