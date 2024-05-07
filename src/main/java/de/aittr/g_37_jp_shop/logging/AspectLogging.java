9fe5ad453050930c8e66b89811ce7adae000e8a3
Andrey <k-udm@ya.ru>
2024-05-07 21:13:39 +0200

    lesson 15

--- src/main/java/de/aittr/g_37_jp_shop/logging/AspectLogging.java
+++ src/main/java/de/aittr/g_37_jp_shop/logging/AspectLogging.java
@@ -1,10 +1,15 @@
 package de.aittr.g_37_jp_shop.logging;
 
+import de.aittr.g_37_jp_shop.domain.dto.ProductDto;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 // AspectJ framework
 @Aspect
@@ -47,5 +52,27 @@ public class AspectLogging {
     public void afterThrowingAnExceptionWhileGettingProductById(Exception e) {
         logger.warn("Method getById of the class ProductServiceImpl threw an exception while getting product: " +
                 "message - {}", e.getMessage());
+    }
+
+    @Pointcut("execution(* de.aittr.g_37_jp_shop.service.ProductServiceImpl.getAll(..))")
+    public void getAllProducts() {}
+
+    @Around("getAllProducts()")
+    public Object aroundGettingAllProducts(ProceedingJoinPoint joinPoint) {
+        logger.info("Method getAll of the class ProductServiceImpl called");
+
+        List<ProductDto> result = null;
+
+        try {
+            result = ((List<ProductDto>) joinPoint.proceed())
+                    .stream()
+                    .filter(x -> x.getPrice().compareTo(new BigDecimal(100)) > 0)
+                    .toList();
+        } catch (Throwable e) {
+            logger.error("Method getAll of the class ProductServiceImpl threw exception: {}", e.getMessage());
+        }
+
+        logger.info("Method getAll of the class ProductServiceImpl finished its work");
+        return result;
     }
 }
